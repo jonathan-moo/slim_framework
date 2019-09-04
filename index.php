9dcaf23faa70f59d0caf077b693683f378baf9e2
Jonathan Moo <jonathanmoo@Jonathans-MacBook-Pro.local>
2019-09-04 16:23:23 -0400

    TTH-1: Defining random routes for testing.

--- index.php
+++ index.php
@@ -1,12 +1,46 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set("America/New_York");
 
-$log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('logs/access.log', Monolog\Logger::WARNING));
-$log->addWarning('Foo');
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
-echo "Hello world";
+$app = AppFactory::create();
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+/*
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+ 
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+     $msg = "homepage";
+     $response->getBody()->write($msg);
+     return $response;
+});
+
+$app->get('/contact', function (Request $request, Response $response, $args) {
+    $msg = "contact";
+    $response->getBody()->write($msg);
+    return $response;
+});
+
+$app->run();
+
 
 ?>
